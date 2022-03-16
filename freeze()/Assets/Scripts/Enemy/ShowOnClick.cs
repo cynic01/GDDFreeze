c9e6f20a0c5b69e9163657c8fd9030aad7b79585
Edison Chen <zx.chen@berkeley.edu>
2022-03-15 19:51:22 -0700

    add some animation to UI

--- freeze()/Assets/Scripts/Enemy/ShowOnClick.cs
+++ freeze()/Assets/Scripts/Enemy/ShowOnClick.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class ShowOnClick : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("Put the UI canvas here")]
+    private bool cameraMoving;
     private Canvas enemyUI;
+
+    [SerializeField]
+    [Tooltip("Animation time taken to move the camera")]
+    private float cameraTotalMoveTime = 0.5f;
     
     // Start is called before the first frame update
     void Start()
     {
+        enemyUI = gameObject.transform.Find("EnemyCanvas").GetComponent<Canvas>();
         enemyUI.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnMouseDown() {
+        if (!cameraMoving) {
+            if (!enemyUI.enabled) {
+                enemyUI.enabled = true;
+                Vector3 targetPos = Camera.main.transform.position - new Vector3(5, 0, 0);
+                StartCoroutine(ShiftCamera(targetPos));
+            } else {
+                enemyUI.enabled = false;
+                Vector3 targetPos = Camera.main.transform.position + new Vector3(5, 0, 0);
+                StartCoroutine(ShiftCamera(targetPos));
+            }
+        }
     }
 
-    private void OnMouseDown() {
-        Debug.Log("clicked on obj");
-        if (!enemyUI.enabled) {
-            enemyUI.enabled = true;
-            Camera.main.transform.position += new Vector3(-5, 0, 0);
-        } else {
-            enemyUI.enabled = false;
-            Camera.main.transform.position += new Vector3(5, 0, 0);
+    IEnumerator ShiftCamera(Vector3 pos) {
+        float timeElapsed = 0f;
+        cameraMoving = true;
+        while (timeElapsed <= cameraTotalMoveTime) {
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, pos, timeElapsed / cameraTotalMoveTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
         }
+        cameraMoving = false;
     }
 }
